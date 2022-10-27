52303632240964278dd0451a28fea27d17c98541
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2022-10-27 13:28:55 +0300

    First changes to quizController

--- src/main/java/com/example/ohjelmointiprojekti1/web/QuizController.java
+++ src/main/java/com/example/ohjelmointiprojekti1/web/QuizController.java
@@ -1,5 +1,13 @@
 package com.example.ohjelmointiprojekti1.web;
 
-public class QuizController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class QuizController {
+	
+	@RequestMapping("/quizzes")
+	public String quizzes() {
+		return "Hello World!";
+	}
 }
