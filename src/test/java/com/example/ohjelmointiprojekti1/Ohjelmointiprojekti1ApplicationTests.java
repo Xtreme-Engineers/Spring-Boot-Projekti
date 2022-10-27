f4854438bbaf340abf529d3e5d8c323295de20f2
CoderGuy-0 <Karri.helokumpu@hotmail.com>
2022-10-27 12:02:16 +0300

    First Commit

---
+++ src/test/java/com/example/ohjelmointiprojekti1/Ohjelmointiprojekti1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmointiprojekti1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ohjelmointiprojekti1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
