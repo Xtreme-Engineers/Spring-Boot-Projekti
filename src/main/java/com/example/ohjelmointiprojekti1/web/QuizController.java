f9786a7631c5fcc67f71bf3ea0cfc5480b98b8f9
Joona Teuho <joona.teuho@gmail.com>
2022-11-07 11:02:27 +0200

    modified quizcontroller

--- src/main/java/com/example/ohjelmointiprojekti1/web/QuizController.java
+++ src/main/java/com/example/ohjelmointiprojekti1/web/QuizController.java
@@ -1,20 +1,16 @@
 package com.example.ohjelmointiprojekti1.web;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.ohjelmointiprojekti1.domain.Question;
 import com.example.ohjelmointiprojekti1.domain.QuestionRepository;
+import com.example.ohjelmointiprojekti1.domain.Quiz;
 import com.example.ohjelmointiprojekti1.domain.QuizRepository;
 
 @CrossOrigin
@@ -26,27 +22,45 @@ public class QuizController {
 	@Autowired
 	private QuizRepository quizrepository;
 
-	//	list all questions on /homepage
+	// list all questions on homepage
+
+	// list all questions on /homepage
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String listQuestions(Model model) {
 		model.addAttribute("questions", questionrepository.findAll());
+		model.addAttribute("quizzes", quizrepository.findAll());
+		model.addAttribute("quiz", new Quiz());
 		return "homepage";
 	}
-	
-	//	add new question
-	@RequestMapping(value = "/add")
-	public String addQuestion(Model model) {
-		model.addAttribute("question", new Question());
-		model.addAttribute("quizzes", quizrepository.findAll());
-		return "addquestion";
+
+	// add new question
+	@RequestMapping(value = "/add/{id}")
+	public String addQuestion(@PathVariable("id") Long quizId, Model model) {
+		Quiz quiz = quizrepository.findById(quizId).get();
+		// model.addAttribute("parentquiz", quiz);
+		Question question = new Question();
+		question.setQuiz(quiz);
+		model.addAttribute("question", question);
+		System.out.print(quiz);
+		return "/addquestion";
 	}
-	
+
 	// save the new question and redirect to /homepage
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveQuestion(Question question) {
-    	System.out.println(question);
+		System.out.println(question);
 		questionrepository.save(question);
 		return "redirect:/";
 
 	}
+
+	@RequestMapping(value = "/savequiz", method = RequestMethod.POST)
+	public String saveQuiz(Quiz quiz) {
+		System.out.println(quiz);
+		quizrepository.save(quiz);
+		return "redirect:/add/1";
+
+	}
+
 }
