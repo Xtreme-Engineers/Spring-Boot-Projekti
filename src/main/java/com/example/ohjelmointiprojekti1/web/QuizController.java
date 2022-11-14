c9eb21b4b99681db5f9683c1d5524ea2822f8799
Miicca <awikkamiikka@gmail.com>
2022-11-14 09:40:11 +0200

    React sivusto

--- src/main/java/com/example/ohjelmointiprojekti1/web/QuizController.java
+++ src/main/java/com/example/ohjelmointiprojekti1/web/QuizController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -78,5 +79,11 @@ public class QuizController {
 	public @ResponseBody Optional<Question> findQuestionRest(@PathVariable("id") Long questionId) {
 		return questionrepository.findById(questionId);
 
+	}
+	
+	// Rest sivusto
+	@GetMapping(value="/rest")
+	public String restSivu(Model model) {
+		return "rest";
 	}
 }
