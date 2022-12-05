9acecdbc85c4643a4ad65c93e83e526e6977fc52
Miicca <awikkamiikka@gmail.com>
2022-12-05 08:45:41 +0200

    Esimerkkikysely

--- src/main/java/com/example/ohjelmointiprojekti1/Ohjelmointiprojekti1Application.java
+++ src/main/java/com/example/ohjelmointiprojekti1/Ohjelmointiprojekti1Application.java
@@ -28,15 +28,55 @@ public class Ohjelmointiprojekti1Application {
 
 		return (args) -> {
 			log.info("Save example quizzes");
-			Quiz qz1 = new Quiz("quiz1");
+			Quiz qz1 = new Quiz("Hyvinvointi kysely");
 			quirepository.save(qz1);
 
 			log.info("Save example questions");
-			Question q1 = new Question("question1", qz1);
+			Question q1 = new Question("Ikä:", qz1);
 			querepository.save(q1);
+			Question q2 = new Question("Sukupuoli:", qz1);
+			querepository.save(q2);
+			Question q3 = new Question("Opiskeluvuosi:", qz1);
+			querepository.save(q3);
+			Question q4 = new Question("Oletko tyytyväinen tämänhetkiseen opintomenestykseesi?", qz1);
+			querepository.save(q4);
+			Question q5 = new Question("Tuntuuko, että olet oikealla alalla?", qz1);
+			querepository.save(q5);
+			Question q6 = new Question("Tunnetko olevasi väsynyt opintojen kanssa?", qz1);
+			querepository.save(q6);
+			Question q7 = new Question("Onko sinulla ollut mielenterveysongelmia opintojen aikana?", qz1);
+			querepository.save(q7);
+			Question q8 = new Question("jos vastasit kyllä, minkälaisia ongelmia?", qz1);
+			querepository.save(q8);
+			Question q9 = new Question("Koetko, että ongelmat ovat vaikuttaneet opintomenestykseen?", qz1);
+			querepository.save(q9);
+			Question q10 = new Question("oetko yksinäisyyttä koulussa?", qz1);
+			querepository.save(q10);
+			Question q11 = new Question("Mitä kehitysideoita sinulla olisi koululle liittyen mielenterveysongelmien ja yksinäisyyden kannalta?", qz1);
+			querepository.save(q11);
 			log.info("Save example answers");
-			Answer a1 = new Answer("answer1", q1);
+			Answer a1 = new Answer("22", q1);
 			arepository.save(a1);
+			Answer a2 = new Answer("Mies", q2);
+			arepository.save(a2);
+			Answer a3= new Answer("2", q3);
+			arepository.save(a3);
+			Answer a4 = new Answer("Kyllä", q4);
+			arepository.save(a4);
+			Answer a5 = new Answer("Kyllä", q5);
+			arepository.save(a5);
+			Answer a6 = new Answer("En", q6);
+			arepository.save(a6);
+			Answer a7 = new Answer("Ei ole", q7);
+			arepository.save(a7);
+			Answer a8 = new Answer("Tyhjä / ei näy, jos ei vastaus kysymyseen 6 on ei", q8);
+			arepository.save(a8);
+			Answer a9 = new Answer("Tyhjä / ei näy, jos ei vastaus kysymyseen 6 on ei", q9);
+			arepository.save(a9);
+			Answer a10 = new Answer("En", q10);
+			arepository.save(a10);
+			Answer a11 = new Answer("Ei ideoita / voi jättää tyhjäksi", q11);
+			arepository.save(a11);
 
 			log.info("Vastaukset");
 			for (Answer answer : arepository.findAll()) {
